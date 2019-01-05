e049da7e7b98a9982de42caeae57bedc81a6c522
Femi <llordbanks500@gmail.com>
2019-01-05 17:26:32 +0100

    Added logic for creating and handling alerts

--- app.js
+++ app.js
@@ -50,7 +50,22 @@ class UI {
         list.appendChild(row)
     }
 
-    
+    static showAlerts(message, className, position) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        const table = document.querySelector('#book-table');
+        if(position === 'upper') {
+            container.insertBefore(div, form);
+        } else {
+            container.insertBefore(div, table);
+        }
+
+        // Prompt to vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
 
     static clearFields() {
         document.querySelector('#title').value = '';
@@ -83,7 +98,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
     // Validate
     if(title === '' || author === '' || isbn === '') {
-        alert('Please fill in all fields!')
+        UI.showAlerts('Please fill in all fields', 'warning', 'upper');
     } else {
         // Instantiate book
         const book = new Book(title, author, isbn); 
@@ -91,6 +106,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         // Add book to list
         UI.addBookToList(book);
     
+        // Show Success Message
+        UI.showAlerts('Book Added', 'success', 'lower');
+
         // Clear fields
         UI.clearFields();
     }
@@ -100,4 +118,5 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+    UI.showAlerts('Book Removed', 'danger', 'lower');
 });
