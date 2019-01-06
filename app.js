ca80d33806a35a5bfa90467f0e637a40f0b6dc96
Femi <llordbanks500@gmail.com>
2019-01-06 21:59:06 +0100

    Created Store class with methods to interact with local storage

--- app.js
+++ app.js
@@ -80,8 +80,36 @@ class UI {
     } 
 }
 
-// Store Class: Handles Storage
+// Store Class: Handles Storage    // local storage only stores things as strings
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books =[];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
 
+        return books;
+    }
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
